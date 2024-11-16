0114d6de2621581d314495e292d5f668d37216fc
Henry Græsberg (Mac) <henrygr@stud.ntnu.no>
2024-11-16 16:14:52 +0100

    Display chosen player in dom rather than alert

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,26 @@ import { useState } from 'react'
 import Link from 'next/link'
 
 const pickRandom = (activePlayers: boolean[]) => {
+  if (activePlayers.length === 0) return null
+
   let activePlayerIndexes: number[] = []
   activePlayers.forEach((active, index) => active ? activePlayerIndexes = [...activePlayerIndexes, index] : null)
 
-  return activePlayerIndexes[Math.floor(Math.random() * activePlayerIndexes.length)]
+  const randomPlayer = activePlayerIndexes[Math.floor(Math.random() * activePlayerIndexes.length)]
+
+  return randomPlayer
 }
 
 const Home = () => {
   const [activePlayers, setActivePlayers] = useState([true, true, true])
+  const [selectedPlayer, setSelectedPlayer] = useState<number | null>(null)
   
   return (
   <div className='h-screen flex flex-col'>
   <header className='bg-slate-800 text-green-300 text-center p-4 mb-10'>
     <h1>!!Work in progess!!(clearly) Buckshot roulette player picker !!Work in progess!!</h1>
   </header>
-  <main className='grid grid-rows-3 grid-cols-3 justify-items-center flex-1'>
+  <main className='grid grid-rows-4 grid-cols-3 justify-items-center flex-1'>
     <div 
     className={`${activePlayers[0] ? 'bg-slate-800' : 'bg-slate-300'} text-green-300 w-16 h-16 row-start-2 col-start-1`}
     onClick={() => {
@@ -49,7 +54,10 @@ const Home = () => {
       3
     </div>
 
-    <button className='row-start-3 col-span-3' onClick={() => alert(`Player ${pickRandom(activePlayers) + 1} has been selected!`)}>Pick a player</button>
+    <button className='row-start-3 col-span-3' onClick={() => setSelectedPlayer(pickRandom(activePlayers))}>Pick a player</button>
+    <div className='row-start-4 col-span-3'>
+      {selectedPlayer != null && <p>Player nr. <span className='font-bold text-red-600'>{selectedPlayer + 1}</span> picked</p>}
+    </div>
   </main>
   <footer className='bg-slate-800 text-green-300 text-center p-4 mt-10'>
     <p>A little &quot;Fuck it, lets have fun&quot; side project by <Link className='underline' href="https://graesberg.com" target='_blank'>Henry Græsberg</Link></p>
