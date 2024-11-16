bdc118a531272f51e95a96f7da764478c13009c3
Henry Græsberg (Mac) <henrygr@stud.ntnu.no>
2024-11-16 15:12:09 +0100

    Add WiP warning and link to source code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { useState } from 'react'
+import Link from 'next/link'
 
 const pickRandom = (activePlayers: boolean[]) => {
   let activePlayerIndexes: number[] = []
@@ -12,7 +13,11 @@ const Home = () => {
   const [activePlayers, setActivePlayers] = useState([true, true, true])
   
   return (
-   <main className='grid grid-rows-3 grid-cols-3 justify-items-center'>
+  <div className='h-screen flex flex-col'>
+  <header className='bg-slate-800 text-green-300 text-center p-4 mb-10'>
+    <h1>!!Work in progess!!(clearly) Buckshot roulette player picker !!Work in progess!!</h1>
+  </header>
+  <main className='grid grid-rows-3 grid-cols-3 justify-items-center flex-1'>
     <div 
     className={`${activePlayers[0] ? 'bg-slate-800' : 'bg-slate-300'} text-green-300 w-16 h-16 row-start-2 col-start-1`}
     onClick={() => {
@@ -45,7 +50,12 @@ const Home = () => {
     </div>
 
     <button className='row-start-3 col-span-3' onClick={() => alert(`Player ${pickRandom(activePlayers) + 1} has been selected!`)}>Pick a player</button>
-   </main> 
+  </main>
+  <footer className='bg-slate-800 text-green-300 text-center p-4 mt-10'>
+    <p>A little &quot;Fuck it, lets have fun&quot; side project by <Link className='underline' href="https://graesberg.com" target='_blank'>Henry Græsberg</Link></p>
+    <Link href="https://git.graesberg.com/buckshot-roulette-randomizer" target='_blank' className='underline'>Source code</Link>
+  </footer>
+  </div>
   );
 }
 
