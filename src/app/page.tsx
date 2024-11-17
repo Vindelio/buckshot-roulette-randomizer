05d453e3e6f8742412ab35c7c5aafdee785abaad
Henry Græsberg <henrygr@stud.ntnu.no>
2024-11-17 01:13:17 +0100

    add self as player, false by default

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ const pickRandom = (activePlayers: boolean[]) => {
 }
 
 const Home = () => {
-  const [activePlayers, setActivePlayers] = useState([true, true, true])
+  const [activePlayers, setActivePlayers] = useState([true, true, true, false])
   const [selectedPlayer, setSelectedPlayer] = useState<number | null>(null)
   
   return (
@@ -22,8 +22,8 @@ const Home = () => {
   <header className='bg-slate-800 text-green-300 text-center p-4 mb-10'>
     <h1>!!Work in progess!!(clearly) Buckshot roulette player picker !!Work in progess!!</h1>
   </header>
-  <main className='grid grid-rows-5 grid-cols-3 justify-items-center items-center flex-1'>
-    <div 
+  <main className='grid grid-rows-6 grid-cols-3 justify-items-center items-center flex-1'>
+    <button 
     className={`${activePlayers[0] ? 'bg-slate-800' : 'bg-slate-300'} ${selectedPlayer === 0 && 'outline'} outline-4 outline-green-500 text-green-300 w-16 h-16 row-start-2 col-start-1 grid place-content-center`}
     onClick={() => {
       const newState = [...activePlayers]
@@ -32,8 +32,8 @@ const Home = () => {
     }}
     >
       1
-    </div>
-    <div
+    </button>
+    <button
     className={`${activePlayers[1] ? 'bg-slate-800' : 'bg-slate-300'} ${selectedPlayer === 1 && 'outline'} outline-4 outline-green-500 text-green-300 w-16 h-16 row-start-1 col-start-2 grid place-content-center`}
     onClick={() => {
       const newState = [...activePlayers]
@@ -42,8 +42,8 @@ const Home = () => {
     }}
     >
       2
-    </div>
-    <div
+    </button>
+    <button
     className={`${activePlayers[2] ? 'bg-slate-800' : 'bg-slate-300'} ${selectedPlayer === 2 && 'outline'} outline-4 outline-green-500 text-green-300 w-16 h-16 row-start-2 col-start-3 grid place-content-center`}
     onClick={() => {
       const newState = [...activePlayers]
@@ -52,9 +52,19 @@ const Home = () => {
     }}
     >
       3
-    </div>
+    </button>
+    <button
+    className={`${activePlayers[3] ? 'bg-slate-800' : 'bg-slate-300'} ${selectedPlayer === 3 && 'outline'} outline-4 outline-green-500 text-green-300 w-16 h-16 row-start-3 col-start-2 grid place-content-center`}
+    onClick={() => {
+      const newState = [...activePlayers]
+      newState[3] = !activePlayers[3]
+      setActivePlayers(newState)
+    }}
+    >
+      4
+    </button>
 
-    <button className='row-start-3 col-span-3 rounded-md w-36 p-4 bg-slate-800' onClick={() => {
+    <button className='row-start-4 col-span-3 rounded-md w-36 p-4 bg-slate-800' onClick={() => {
       setSelectedPlayer(null)
       for (let i = 0; i < 1000; i++)
       setTimeout(() => {setSelectedPlayer(pickRandom(activePlayers))}, Math.floor(Math.random() * 500 + 300))}
@@ -62,14 +72,14 @@ const Home = () => {
       Pick a player
     </button>
 
-    <button className='row-start-4 col-span-3 rounded-md w-36 p-4 bg-slate-800' onClick={() => {
+    <button className='row-start-5 col-span-3 rounded-md w-36 p-4 bg-slate-800' onClick={() => {
       setSelectedPlayer(null)
       setActivePlayers([true, true, true])
     }}>
       Reset
     </button>
 
-    <div className='row-start-5 col-span-3'>
+    <div className='row-start-6 col-span-3'>
       {selectedPlayer != null && <p>Player nr. <span className='font-bold text-red-600'>{selectedPlayer + 1}</span> picked</p>}
     </div>
   </main>
