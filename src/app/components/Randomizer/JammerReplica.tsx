ce57db03922978298601c03055df13ebd9762781
Henry Græsberg <henrygr@stud.ntnu.no>
2024-11-22 21:12:47 +0100

    Resize jammer parts

--- src/app/components/Randomizer/JammerReplica.tsx
+++ src/app/components/Randomizer/JammerReplica.tsx
@@ -33,46 +33,47 @@ const JammerReplica = () => {
   const angles = ["-rotate-90", "", "rotate-90", "rotate-180"]
 
   return (
-    <main className='flex flex-col justify-items-center items-center gap-10 aspect-[365/455] bg-[#200000] p-4'>
-      <div className='w-full'>
-        <h2 className='text-lg'>graesberg electronics</h2>
+    <main className='grid justify-items-center items-center aspect-[365/455] bg-[#200000] p-4'>
+      <div className='flex w-full h-full flex-col justify-start'>
+        <div className='w-full'>
+          <h2 className='text-lg'>graesberg electronics</h2>
+        </div>
+        <Screen selectedPlayer={selectedPlayer} className='max-w-full' />
       </div>
 
-      <Screen selectedPlayer={selectedPlayer} />
-
-      <div className='flex justify-evenly w-full'>
-        {activePlayers.map((active, index) => (
-          <PlayerButton
-            key={index}
-            active={active}
-            onclick={() => handlePlayerSelect(activePlayers, setActivePlayers, index)}
-            disabled={running}
+      <div className='flex flex-col gap-10 w-full'>
+        <div className='flex justify-evenly w-full'>
+          {activePlayers.map((active, index) => (
+            <PlayerButton
+              key={index}
+              active={active}
+              onclick={() => handlePlayerSelect(activePlayers, setActivePlayers, index)}
+              disabled={running}
+            >
+              <Image src={arrow} alt="Player arrow" className={`${angles[index]} w-7`} />
+            </PlayerButton>
+          ))}
+        </div>
+        <div className='flex justify-between w-full'>
+          <WideButton onClick={() => {
+            const interval = setInterval(() => {setSelectedPlayer(pickRandom(activePlayers))}, Math.floor(Math.random() * 100))
+            setRunning(true)
+            setTimeout(() => {clearInterval(interval); setRunning(false)}, Math.floor(Math.random() * 10 * 500 + 300))
+          }}
+          {...(activePlayers.every(active => !active) ? {disabled: true} : {})}
+          disabled={running}
           >
-            <Image src={arrow} alt="Player arrow" className={`${angles[index]} w-7`} />
-          </PlayerButton>
-        ))}
-      </div>
-
-      <div className='flex justify-between w-full'>
-        <WideButton onClick={() => {
-          const interval = setInterval(() => {setSelectedPlayer(pickRandom(activePlayers))}, Math.floor(Math.random() * 100))
-          setRunning(true)
-          setTimeout(() => {clearInterval(interval); setRunning(false)}, Math.floor(Math.random() * 10 * 500 + 300))
-        }}
-        {...(activePlayers.every(active => !active) ? {disabled: true} : {})}
-        disabled={running}
-        >
-          Pick a player
-        </WideButton>
-
-        <WideButton onClick={() => {
-          setSelectedPlayer(null)
-          setActivePlayers([true, true, true, false])
-        }}
-        disabled={running}
-        >
-          Reset
-        </WideButton>
+            Pick a player
+          </WideButton>
+          <WideButton onClick={() => {
+            setSelectedPlayer(null)
+            setActivePlayers([true, true, true, false])
+          }}
+          disabled={running}
+          >
+            Reset
+          </WideButton>
+        </div>
       </div>
     </main>
   )
--- src/app/components/Randomizer/JammerScreen.tsx
+++ src/app/components/Randomizer/JammerScreen.tsx
@@ -4,6 +4,7 @@ import arrow from "@/images/arrow.svg"
 
 interface JammerScreenProps {
   selectedPlayer: number | null
+  className?: string
 }
 
 interface ScreenPartProps {
@@ -16,16 +17,18 @@ const angles = ["-rotate-90", "", "rotate-90", "rotate-180"]
 
 const ScreenPart: FC<ScreenPartProps> = ({ className, selected, rotate }) => (
   <div
-    className={` ${rotate ? "aspect-[147/67] h-8" : "aspect-[67/147] w-8"} ${selected ? 'bg-green-500' : 'bg-green-950'} ${className}`}
+    className={` ${rotate ? "aspect-[147/67] w-full" : "aspect-[67/147] h-full"} ${selected ? 'bg-green-500' : 'bg-green-950'} ${className}`}
   ></div>
 )
 
-const JammerScreen: FC<JammerScreenProps> = ({ selectedPlayer }) => (
-  <div className="grid">
-    <ScreenPart selected={selectedPlayer === 0} className="col-start-1 row-start-2" />
-    <ScreenPart selected={selectedPlayer === 1} className="col-start-2 row-start-1" rotate />
-    <ScreenPart selected={selectedPlayer === 2} className="col-start-3 row-start-2" />
-    <ScreenPart selected={selectedPlayer === 3} className="col-start-2 row-start-3" rotate />
+// Component overflows in the x axis
+//TODO: Figure out why and fix
+const JammerScreen: FC<JammerScreenProps> = ({ selectedPlayer, className }) => (
+  <div className={`grid h-full w-full aspect-square grid-rows-3 grid-cols-3 ${className}`}> 
+    <ScreenPart selected={selectedPlayer === 0} className="col-start-1 row-start-2 justify-self-end self-center" />
+    <ScreenPart selected={selectedPlayer === 1} className="col-start-2 row-start-1 justify-self-center self-end" rotate />
+    <ScreenPart selected={selectedPlayer === 2} className="col-start-3 row-start-2 self-center" />
+    <ScreenPart selected={selectedPlayer === 3} className="col-start-2 row-start-3 justify-self-center" rotate />
 
     <Image
     src={arrow}
