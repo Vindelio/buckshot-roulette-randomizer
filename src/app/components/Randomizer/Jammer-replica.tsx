cacdc70b43ee77c75c28c88d03166040778ed91f
Henry Græsberg (Mac) <henrygr@stud.ntnu.no>
2024-11-22 03:44:58 +0100

    Obey DRY

--- src/app/components/Randomizer/Jammer-replica.tsx
+++ src/app/components/Randomizer/Jammer-replica.tsx
@@ -27,40 +27,24 @@ const JammerReplica = () => {
   const [activePlayers, setActivePlayers] = useState([true, true, true, false])
   const [selectedPlayer, setSelectedPlayer] = useState<number | null>(null)
 
+  const angles = ["-rotate-90", "", "rotate-90", "rotate-180"]
+
   return (
     <main className='flex flex-col justify-items-center items-center gap-10 aspect-[365/455] bg-[#200000] p-4'>
       <div className='w-full'>
         <h2 className='text-lg'>graesberg electronics</h2>
       </div>
       <div className='flex justify-evenly w-full'>
-        <PlayerButton
-          active={activePlayers[0]}
-          chosen={selectedPlayer === 0}
-          onclick={() => handlePlayerSelect(activePlayers, setActivePlayers, 0)}
-        >
-          <Image src={arrow} alt="Player arrow" className='-rotate-90 w-7' />
-        </PlayerButton>
-        <PlayerButton
-          active={activePlayers[1]}
-          chosen={selectedPlayer === 1}
-          onclick={() => handlePlayerSelect(activePlayers, setActivePlayers, 1)}
-        >
-          <Image src={arrow} alt="Player arrow" className='w-7' />
-        </PlayerButton>
-        <PlayerButton
-          active={activePlayers[2]}
-          chosen={selectedPlayer === 2}
-          onclick={() => handlePlayerSelect(activePlayers, setActivePlayers, 2)}
-        >
-          <Image src={arrow} alt="Player arrow" className='rotate-90 w-7' />
-        </PlayerButton>
-        <PlayerButton
-          active={activePlayers[3]}
-          chosen={selectedPlayer === 3}
-          onclick={() => handlePlayerSelect(activePlayers, setActivePlayers, 3)}
-        >
-          <Image src={arrow} alt="Player arrow" className='rotate-180 w-7' />
-        </PlayerButton>
+        {activePlayers.map((active, index) => (
+          <PlayerButton
+            key={index}
+            active={active}
+            chosen={selectedPlayer === index}
+            onclick={() => handlePlayerSelect(activePlayers, setActivePlayers, index)}
+          >
+            <Image src={arrow} alt="Player arrow" className={`${angles[index]} w-7`} />
+          </PlayerButton>
+        ))}
       </div>
 
       <div className='flex justify-between w-full'>
@@ -70,6 +54,7 @@ const JammerReplica = () => {
           setTimeout(() => {setSelectedPlayer(pickRandom(activePlayers))}, Math.floor(Math.random() * 500 + 300))}}>
           Pick a player
         </WideButton>
+
         <WideButton onClick={() => {
           setSelectedPlayer(null)
           setActivePlayers([true, true, true, false])
