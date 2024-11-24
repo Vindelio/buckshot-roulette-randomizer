291210f95db610877ada4bdb5576aa7ca49b0a49
Henry Græsberg (Mac) <henrygr@stud.ntnu.no>
2024-11-24 13:02:15 +0100

    Make buttons wider

--- src/app/components/Randomizer/JammerWideButton.tsx
+++ src/app/components/Randomizer/JammerWideButton.tsx
@@ -9,7 +9,7 @@ interface ButtonProps extends React.ComponentPropsWithoutRef<"button"> {
 
 const WideButton: FC<ButtonProps> = ({ className, onClick, children, disabled, ...props }) => (
   <button
-  className={`rounded-md min-w-20 w-[20%] p-4 bg-slate-200 text-slate-950 disabled:bg-slate-400 ${className}`}
+  className={`rounded-md min-w-20 w-[40%] p-3 bg-slate-200 text-slate-950 disabled:bg-slate-400 ${className}`}
   onClick={onClick}
   disabled={disabled}
   {...props}
